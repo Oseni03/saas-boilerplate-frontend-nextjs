8ec68e01111159ef83d7ba7a0c66a7fe5cecf942
Oseni03 <oseniayomide57@gmail.com>
2024-08-13 08:29:18 +0700

    feat: add basic navbar and hero section

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en">
+      <Header/>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      Hello World
-      <Button>Subscribe</Button>
+      <Hero/>
     </div>
   );
 }
