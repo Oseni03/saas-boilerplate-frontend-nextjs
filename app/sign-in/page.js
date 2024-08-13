7113fe3e430f4da40f816d3f2e0c59da4a42640e
Oseni03 <oseniayomide57@gmail.com>
2024-08-14 06:53:19 +0700

    feat: add sign-in form

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -1,48 +1,50 @@
 import * as React from "react"
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import Image from "next/image"
 
 
 
 export default function Page() {
   return (
-    <Card className="w-[350px] mx-auto my-auto">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-    
-		<form className="max-w-sm mx-auto">
-		<div className="mb-5">
-			<label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-			<input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-		</div>
-		<div className="mb-5">
-			<label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-			<input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-		</div>
-		<div className="flex items-start mb-5">
-			<div className="flex items-center h-5">
-			<input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-			</div>
-			<label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-		</div>
-		</form>
-
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button type="submit">Deploy</Button>
-      </CardFooter>
-    </Card>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+              <Image className="w-8 h-8 mr-2" src={"/public/next.svg"} alt="logo" width={100} height={100}/>
+              Flowbite    
+          </a>
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Sign in to your account
+                  </h1>
+                  <form className="space-y-4 md:space-y-6" action="#">
+                      <div>
+                          <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600" placeholder="name@company.com" required=""/>
+                      </div>
+                      <div>
+                          <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      </div>
+                      <div className="flex items-center justify-between">
+                          <div className="flex items-start">
+                              <div className="flex items-center h-5">
+                                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary dark:focus:ring-primary" required=""/>
+                              </div>
+                              <div className="ml-3 text-sm">
+                                <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                              </div>
+                          </div>
+                          <a href="#" className="text-sm font-medium text-primary">Forgot password?</a>
+                      </div>
+                      <button type="submit" className="w-full text-white bg-primary focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                          Don’t have an account yet? <a href="#" className="font-medium text-primary">Sign up</a>
+                      </p>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </section>
   )
 }
