bab5fd97e73326d4c5fde697b6c8eb5a2b693241
Oseni03 <oseniayomide57@gmail.com>
2024-08-14 07:28:40 +0700

    feat: add features and social-proof components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Features from "./_components/features";
 import Hero from "./_components/Hero";
+import SocialProof from "./_components/SocialProof";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <Features/>
+      <SocialProof/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,42 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontFamily: {
+        'body': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ],
+        'sans': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ]
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
