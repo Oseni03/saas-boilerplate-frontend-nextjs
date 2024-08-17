7fe870bfbfd34a32b28fd2feea40aa6c8f8a84bd
Oseni03 <oseniayomide57@gmail.com>
2024-08-17 09:57:14 +0100

    refactor: restructure password-reset enpoint

---
+++ app/password-reset/[user]/[token]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Set new password</div>
+  )
+}
+
+export default Page
--- app/password-reset/page.js
+++ app/password-reset/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Page() {
   return (
-    <div>Password Reset Page</div>
+    <div>Request password reset</div>
   )
 }
 
