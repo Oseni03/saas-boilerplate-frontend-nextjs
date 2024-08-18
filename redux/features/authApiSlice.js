88fbda2190fd763def4054ddef8d04a744201219
Oseni03 <oseniayomide57@gmail.com>
2024-08-18 02:44:40 +0100

    feat: add retrieveuser and socialAuth endpoints

---
+++ redux/features/authApiSlice.js
@@ -0,0 +1,23 @@
+import { apiSlice } from "../services/apiSlice";
+
+const authApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		retrieveUser: builder.query({
+			query: () => "/users/me/",
+		}),
+		socialAuthenticate: builder.mutation({
+			query: ({ provider, state, code }) => ({
+				url: `/o/${provider}?state=${encodeURIComponent(
+					state
+				)}&code=${encodeURIComponent(code)}`,
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			}),
+		}),
+	}),
+});
+
+export const { useRetrieveUserQuery } = authApiSlice;
