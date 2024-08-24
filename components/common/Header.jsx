814d6e1e4df0121b66691f222058cabc65145622
Oseni03 <oseniayomide57@gmail.com>
2024-08-24 12:01:41 +0100

    feat: add user dropdown menu to the guest page layout when authenticated

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -11,6 +11,7 @@ import NavigationLinks from "./NavigationLinks";
 import NavLink from "./NavLink";
 import Link from "next/link";
 import NavigationSidebarPanel from "./NavigationSidebarPanel";
+import UserDropdownMenu from "./UserDropdownMenu";
 
 export default function Header() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -71,7 +72,7 @@ export default function Header() {
 				</div>
 				<NavigationLinks />
 				<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-					{isAuthenticated ? authButtons() : guestButtons()}
+					{isAuthenticated ? <UserDropdownMenu /> : guestButtons()}
 				</div>
 			</nav>
 			<Dialog
@@ -82,7 +83,7 @@ export default function Header() {
 				<div className="fixed inset-0 z-10" />
 				<DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
-						<a href="#" className="-m-1.5 p-1.5">
+						<a href="/" className="-m-1.5 p-1.5">
 							<span className="sr-only">Your Company</span>
 							<img
 								alt=""
@@ -99,6 +100,7 @@ export default function Header() {
 							<XMarkIcon aria-hidden="true" className="h-6 w-6" />
 						</button>
 					</div>
+					{isAuthenticated && <UserDropdownMenu />}
 					<div className="mt-6 flow-root">
 						<div className="-my-6 divide-y divide-gray-500/10">
 							<div className="space-y-2 py-6">
@@ -108,9 +110,7 @@ export default function Header() {
 								/>
 							</div>
 							<div className="py-6">
-								{isAuthenticated
-									? authButtons()
-									: guestButtons()}
+								{!isAuthenticated && guestButtons()}
 							</div>
 						</div>
 					</div>
--- components/common/Hero.jsx
+++ components/common/Hero.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 function Hero() {
 	return (
 		<section className="bg-white dark:bg-gray-900">
-			<div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+			<div className="py-14 px-4 mx-auto max-w-screen-xl text-center lg:py-24 lg:px-12">
 				<a
 					href="#"
 					className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
@@ -24,9 +24,9 @@ function Hero() {
 						xmlns="http://www.w3.org/2000/svg"
 					>
 						<path
-							fill-rule="evenodd"
+							fillRule="evenodd"
 							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-							clip-rule="evenodd"
+							clipRule="evenodd"
 						></path>
 					</svg>
 				</a>
@@ -41,7 +41,7 @@ function Hero() {
 				<div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
 					<a
 						href="#"
-						className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+						className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary focus:ring-4"
 					>
 						Learn more
 						<svg
@@ -51,9 +51,9 @@ function Hero() {
 							xmlns="http://www.w3.org/2000/svg"
 						>
 							<path
-								fill-rule="evenodd"
+								fillRule="evenodd"
 								d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-								clip-rule="evenodd"
+								clipRule="evenodd"
 							></path>
 						</svg>
 					</a>
@@ -72,7 +72,7 @@ function Hero() {
 						Watch video
 					</a>
 				</div>
-				<div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+				<div className="px-4 pt-6 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
 					<span className="font-semibold text-gray-400 uppercase">
 						FEATURED IN
 					</span>
