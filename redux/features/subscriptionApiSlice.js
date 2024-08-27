94a0184a26facee83aade469d23befa558dc8026
Oseni03 <oseniayomide57@gmail.com>
2024-08-27 10:54:17 +0100

    feat: add subscription and pricing endpoints

---
+++ redux/features/subscriptionApiSlice.js
@@ -0,0 +1,49 @@
+import { apiSlice } from "../services/apiSlice";
+
+const settingsApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		retrievePrices: builder.query({
+			query: () => `/pricing/`,
+		}),
+		createCheckout: builder.mutation({
+			query: ({ price_id }) => ({
+				url: `/subscriptions/create-checkout/`,
+				method: "POST",
+				body: { price_id },
+			}),
+		}),
+		finalizeCheckout: builder.mutation({
+			query: ({ session_id }) => ({
+				url: `/subscriptions/finalize-checkout/`,
+				method: "POST",
+				body: { session_id },
+			}),
+		}),
+		cancelSubscription: builder.mutation({
+			query: () => ({
+				url: `/subscriptions/cancel/`,
+				method: "PUT",
+				body: {},
+			}),
+		}),
+		createCustomerPortal: builder.mutation({
+			query: () => ({
+				url: `/subscriptions/create-customer-portal/`,
+				method: "POST",
+				body: {},
+			}),
+		}),
+		retrieveSubscription: builder.query({
+			query: () => `/subscriptions/`,
+		}),
+	}),
+});
+
+export const {
+	useRetrievePricesQuery,
+	useCreateCheckoutMutation,
+	useFinalizeCheckoutMutation,
+	useCancelSubscriptionMutation,
+	useCreateCustomerPortalMutation,
+	useRetrieveSubscriptionQuery,
+} = settingsApiSlice;
